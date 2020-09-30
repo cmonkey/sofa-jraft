a894cefb5ba1301b1cc3bc681789f1cfbace2122
CMonkey <42.codemonkey@gmail.com>
2020-09-30 11:30:17 +0800

    add main method to CounterClientBootstrap

--- jraft-example/src/main/java/com/alipay/sofa/jraft/example/counter/CounterClientBootstrap.java
+++ jraft-example/src/main/java/com/alipay/sofa/jraft/example/counter/CounterClientBootstrap.java
@@ -1,2 +1,7 @@
-package com.alipay.sofa.jraft.example.counter;public class CounterClientBootstrap {
+package com.alipay.sofa.jraft.example.counter;
+
+public class CounterClientBootstrap {
+    public static void main(String[] args) {
+
+    }
 }
